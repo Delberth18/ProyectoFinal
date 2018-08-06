25826f817dfd3247cd5ef73555f606134fa6d91f
Delberth <Delberth@DESKTOP-5BN6HJ9>
2018-08-06 07:37:51 -0600

    cambio para lo de precios

--- SurfTribeSystem1.0/Site1.Master.designer.cs
+++ SurfTribeSystem1.0/Site1.Master.designer.cs
@@ -40,6 +40,15 @@ namespace SurfTribeSystem1._0 {
         protected global::System.Web.UI.HtmlControls.HtmlForm f2;
         
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
         /// GuanaRepeat control.
         /// </summary>
         /// <remarks>
